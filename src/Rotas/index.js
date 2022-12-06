68885639511994090b1a10480f4fd13759948b8d
Diego Melo <diegoo-melo@uni9.edu.br>
2022-12-06 19:46:16 -0300

    prj

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 import PaginaInicial from '../paginas/PaginaInicial'
 import PaginaFrontEnd from '../paginas/PaginaFrontEnd';
 import PaginaBackEnd from '../paginas/PaginaBackEnd';
 
 const Rotas = () => (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
-            <Route path='/' element={ <PaginaInicial/> } />
+            <Route exact path='/' element={ <PaginaInicial/> } />
             <Route path='/front-end' element={ <PaginaFrontEnd/> } />
             <Route path='/back-end' element={ <PaginaBackEnd/> } />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default Rotas;
